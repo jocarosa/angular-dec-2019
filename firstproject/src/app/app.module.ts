0f1475aeb4e43308168f340cc332b8cd6b535f8f
unknown <jocarosa@github.com>
2020-02-12 17:51:31 -0800

    s

--- firstproject/src/app/app.module.ts
+++ firstproject/src/app/app.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { AppRoutingModule } from '@app/app-routing.module';
 import { AppComponent } from '@app/app.component';
-
-
-import {PokemonService} from '@pokemon/pokemon.service';
+import { PokemonService } from '@pokemon/pokemon.service';
 import { HttpClientModule,HttpClient } from '@angular/common/http';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { ModalModule } from 'ngx-bootstrap/modal';
